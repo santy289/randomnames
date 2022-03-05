df034474359a49763985c1f5ce1d0cff598308de
Santy <santiago.valencia4@udea.edu.co>
2022-03-04 19:03:26 -0500

    feature:error-msg

--- RandomNames.js
+++ RandomNames.js
@@ -1,7 +1,15 @@
 const fs = require('fs');
 const { faker } = require('@faker-js/faker');
 
-fs.writeFileSync('Fake-Names.txt', 'Random Names' + "\r\n\r\n") ;
+fs.writeFileSync('Fake-Names.txt', 'Random Names' + "\r\n\r\n",function(err,data){
+    if (err){
+        console.log ("Error.");
+    }
+        else{
+        console.log(data);
+        }
+    }
+);
 
 for (i = 0; i<1001; i++){
     const randomName = faker.name.findName();
